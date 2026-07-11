6a0c2e9e8ca4e3b6a03ef9cce8d070a9655d2ae5
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-07-11 18:08:17 +1000

    cors added to allow endpoints to call backend, currently set to allow all endpoints to contact backend

--- core/settings.py
+++ core/settings.py
@@ -74,7 +74,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
