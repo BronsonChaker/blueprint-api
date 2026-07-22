99e99fcdb0f7050204201c03842f1ab406b7a06b
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-07-22 20:22:44 +1000

    Added change to Jobsa serializer to return first and last name of user for jobs endpoint

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -5,8 +5,8 @@ from users.serializers import UserSerializer
 
 class JobSerializer(serializers.ModelSerializer):
     tasks = TaskSerializer(many=True, read_only=True)
-    first_name = serializers.CharField(source='user.first_name', read_only=True)
-    last_name = serializers.CharField(source='user.last_name', read_only=True)
+    first_name = serializers.CharField(source='supervisor.first_name', read_only=True)
+    last_name = serializers.CharField(source='supervisor.last_name', read_only=True)
     class Meta:
         model = Job
         fields = [
