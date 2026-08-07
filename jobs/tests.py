393b21fad09a9359a933de48261f879c076afcb5
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-08-08 05:41:56 +1000

    Created failure test case for job creation with no organisation

--- jobs/tests.py
+++ jobs/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from .models import Job
 from organisations.models import Organisation
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 User = get_user_model()
 class JobTestCase(TestCase):
@@ -26,6 +27,20 @@ class JobTestCase(TestCase):
 
         self.assertEqual(job.job_number, "12345")
         self.assertEqual(job.organisation, self.organisation)
+        self.assertEqual(job.supervisor, self.user)
+        self.assertEqual(job.name, "123 Test Street, Test, NSW, 1234")
+        self.assertEqual(job.address, "123 Test Street, Test, NSW, 1234")
+
+    def test_create_job_no_org_fails(self):
+    
+        job = Job.objects.create(
+            job_number = "12345",
+            supervisor = self.user,
+            name="123 Test Street, Test, NSW, 1234",
+            address="123 Test Street, Test, NSW, 1234",
+        )
+
+        self.assertEqual(job.job_number, "12345")
         self.assertEqual(job.supervisor, self.user)
         self.assertEqual(job.name, "123 Test Street, Test, NSW, 1234")
         self.assertEqual(job.address, "123 Test Street, Test, NSW, 1234")
