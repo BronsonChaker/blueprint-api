b0869eb1eb3c237afdee61b19eae0db37ce52721
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-08-09 21:32:26 +1000

    Jobs Unit Testing: Additional alterations to unit testing for project creation

--- jobs/tests.py
+++ jobs/tests.py
@@ -23,6 +23,15 @@ class JobTestCase(TestCase):
             supervisor = self.user,
             name="123 Test Street, Test, NSW, 1234",
             address="123 Test Street, Test, NSW, 1234",
+            status="Sales",
+            stage="Active",
+            client_name="John Smith",
+            client_email="johnsmith@gmail.com",
+            client_phone="0412345678",
+            contract_start_date="2026-01-01",
+            contract_end_date="2026-01-02",
+            construction_start_date="2026-01-01",
+            construction_end_date="2026-01-02",
         )
 
         self.assertEqual(job.job_number, "12345")
@@ -30,18 +39,28 @@ class JobTestCase(TestCase):
         self.assertEqual(job.supervisor, self.user)
         self.assertEqual(job.name, "123 Test Street, Test, NSW, 1234")
         self.assertEqual(job.address, "123 Test Street, Test, NSW, 1234")
+        self.assertEqual(job.status, "Sales")
+        self.assertEqual(job.stage, "Active")
+        self.assertEqual(job.client_name, "John Smith")
+        self.assertEqual(job.client_email, "johnsmith@gmail.com")
+        self.assertEqual(job.client_phone, "0412345678")
+        self.assertEqual(job.contract_start_date, "2026-01-01")
+        self.assertEqual(job.contract_end_date, "2026-01-02")
+        self.assertEqual(job.construction_start_date, "2026-01-01")
+        self.assertEqual(job.construction_end_date, "2026-01-02")
+
 
     def test_create_job_no_org_fails(self):
-    
-        job = Job.objects.create(
-            job_number = "12345",
-            supervisor = self.user,
-            name="123 Test Street, Test, NSW, 1234",
-            address="123 Test Street, Test, NSW, 1234",
-        )
+        with self.assertRaises(IntegrityError):
+            job = Job.objects.create(
+                job_number = "12345",
+                supervisor = self.user,
+                name="123 Test Street, Test, NSW, 1234",
+                address="123 Test Street, Test, NSW, 1234",
+            )
 
-        self.assertEqual(job.job_number, "12345")
-        self.assertEqual(job.supervisor, self.user)
-        self.assertEqual(job.name, "123 Test Street, Test, NSW, 1234")
-        self.assertEqual(job.address, "123 Test Street, Test, NSW, 1234")
+            self.assertEqual(job.job_number, "12345")
+            self.assertEqual(job.supervisor, self.user)
+            self.assertEqual(job.name, "123 Test Street, Test, NSW, 1234")
+            self.assertEqual(job.address, "123 Test Street, Test, NSW, 1234")
         
