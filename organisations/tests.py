e202810cf3e121443351c1d1981cef1073997e18
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-08-10 05:49:36 +1000

    Organisation Unit Testing: Started class to create organisation unit testing

--- organisations/tests.py
+++ organisations/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Organisation
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
-# Create your tests here.
+User = get_user_model()
+
+class OrganisationTestCase(TestCase):
+    def organisation_set_up(self):
+        
