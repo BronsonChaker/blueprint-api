675c097bedb9959a517536ad3912327d071cc8ac
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-08-10 20:19:49 +1000

    Organisation Unit Testing: Created function for org unit testing, passess all cases works in test DB

--- organisations/tests.py
+++ organisations/tests.py
@@ -6,5 +6,17 @@ from django.db import IntegrityError
 User = get_user_model()
 
 class OrganisationTestCase(TestCase):
-    def organisation_set_up(self):
-        
+    def test_create_org(self):
+        org = Organisation.objects.create(
+            name="Test Organisation",
+            email_address="test@email.com",
+            billing_address="123 Test Street, NSW, 2749",
+            phone_number="0412345678",
+        )
+
+        self.assertEqual(org.name,"Test Organisation")
+        self.assertEqual(org.email_address,"test@email.com")
+        self.assertEqual(org.billing_address,"123 Test Street, NSW, 2749")
+        self.assertEqual(org.phone_number,"0412345678")
+
+
