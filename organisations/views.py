d3d2562b725bccca3e15cda18fbd5cdc37e26886
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-09-04 07:22:28 +1000

    Added imports to org views, will be used for checking user admin status

--- organisations/views.py
+++ organisations/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import *
 from .models import Organisation
 from users.models import User, Membership
